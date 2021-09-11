20558727c790138d6eb3b898a7198c3cca6f13c8
unamednada <dias.gustavo.dias@gmail.com>
2021-09-10 22:25:37 -0300

    Updates

---
+++ precache-manifest.0d6200ed1e3d4e6c6ce2337ea738e368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b949766d419ba8a4d6c84aa406b3c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0144564c6cc7ab55808e",
+    "url": "/static/css/main.e3377c3c.chunk.css"
+  },
+  {
+    "revision": "648f91b2796d601f1c32",
+    "url": "/static/js/2.f1509ae3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f1509ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0144564c6cc7ab55808e",
+    "url": "/static/js/main.642263e5.chunk.js"
+  },
+  {
+    "revision": "55d82bdc049f947e589b",
+    "url": "/static/js/runtime-main.1003f5cf.js"
+  },
+  {
+    "revision": "c72621847d1b09317793f1fe0f380d2c",
+    "url": "/static/media/bg.c7262184.jpg"
+  }
+]);
