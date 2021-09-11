aa689efa9f1d2aabe3d75c9162c836967ff928c3
unamednada <dias.gustavo.dias@gmail.com>
2021-09-10 22:36:06 -0300

    Updates

---
+++ precache-manifest.4831c06239f0cee79c5749263b7643c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60464b948ba513216eb60e7d6cb950d0",
+    "url": "/exercise-dog-image/index.html"
+  },
+  {
+    "revision": "1a805c9d8533f42a597c",
+    "url": "/exercise-dog-image/static/css/main.ce595ddb.chunk.css"
+  },
+  {
+    "revision": "648f91b2796d601f1c32",
+    "url": "/exercise-dog-image/static/js/2.f1509ae3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/exercise-dog-image/static/js/2.f1509ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a805c9d8533f42a597c",
+    "url": "/exercise-dog-image/static/js/main.b55cd8b4.chunk.js"
+  },
+  {
+    "revision": "10ff0c0d15c7a9b19d88",
+    "url": "/exercise-dog-image/static/js/runtime-main.53ef2e20.js"
+  },
+  {
+    "revision": "c72621847d1b09317793f1fe0f380d2c",
+    "url": "/exercise-dog-image/static/media/bg.c7262184.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d6200ed1e3d4e6c6ce2337ea738e368.js"
+  "/exercise-dog-image/precache-manifest.4831c06239f0cee79c5749263b7643c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/exercise-dog-image/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
